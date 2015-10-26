e721111f3009ca278eb2967caac08efeed553120
Mohamed Diaa <mohamed.diaa27@gmail.com>
2015-10-27 00:29:28 +0200

    Initial Commit

---
+++ RollTheBall/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  RollTheBall
+//
+//  Created by Mohamed Diaa on 10/27/15.
+//  Copyright © 2015 Mohamed Diaa. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
