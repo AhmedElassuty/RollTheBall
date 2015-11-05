bc227b79cb342873036aeb05cdecde1136a41de1
Mohamed Diaa <mohamed.diaa27@gmail.com>
2015-11-05 03:02:41 +0200

    pushing init goal tile pint

--- RollTheBall/Utils/helpers.swift
+++ RollTheBall/Utils/helpers.swift
@@ -81,8 +81,6 @@ func genGrid() -> [[Tile]] {
         pathTileTypes.append(pathTileConfigs.removeFirst())
     }
     
-    
-    
     // Shuffle Types
     for i in 0 ..< (types.count - 1) {
         let j = Int(arc4random_uniform(UInt32(types.count - i))) + i
@@ -120,31 +118,44 @@ func genGrid() -> [[Tile]] {
 }
 
 func visualizeBoard(grid: [[Tile]]){
+    let rightSeparator = "|", leftSeparator = "|", topSeparator = "¯", bottomSeparator = "_"
+    let rightArrow = "→", leftArrow = "←", topArrow = "↑", bottomArrow = "↓"
+    var right = rightSeparator, left = leftSeparator, top = topSeparator, bottom = bottomSeparator
     var horizontalBorder = "|", topHorizontalSpace = "|", bottomHorizontalSpace = "|", horizontalvalues = "|"
     for _ in 0...grid[0].count-1 {
         horizontalBorder += "_______|"
     }
     print(horizontalBorder)
     for row in 0...grid.count-1 {
-        var right = "|", left = "|", top = "¯", bottom = "_"
-        horizontalvalues = "|"
-        topHorizontalSpace = "|"
-        bottomHorizontalSpace = "|"
+        horizontalvalues = "|" ; topHorizontalSpace = "|" ; bottomHorizontalSpace = "|"
         for tile in grid[row] {
             switch tile {
             case is PathTile:
-                right  = (tile as! PathTile).config.contains(.Right)  ? " " : "|"
-                left   = (tile as! PathTile).config.contains(.Left)   ? " " : "|"
-                top    = (tile as! PathTile).config.contains(.Top)    ? " " : "¯"
-                bottom = (tile as! PathTile).config.contains(.Bottom) ? " " : "_"
+                right  = (tile as! PathTile).config.contains(.Right)  ? rightArrow : rightSeparator
+                left   = (tile as! PathTile).config.contains(.Left)   ? leftArrow : leftSeparator
+                top    = (tile as! PathTile).config.contains(.Top)    ? topArrow : topSeparator
+                bottom = (tile as! PathTile).config.contains(.Bottom) ? bottomArrow : bottomSeparator
+                
+            case is GoalTile:
+                right  = (tile as! GoalTile).enterEdge == .Right    ? rightArrow : rightSeparator
+                left   = (tile as! GoalTile).enterEdge == .Left     ? leftArrow  : leftSeparator
+                top    = (tile as! GoalTile).enterEdge == .Top      ? topArrow : topSeparator
+                bottom = (tile as! GoalTile).enterEdge == .Bottom   ? bottomArrow : bottomSeparator
+                
+            case is InitialTile:
+                right  = (tile as! InitialTile).exitEdge == .Right   ? rightArrow : rightSeparator
+                left   = (tile as! InitialTile).exitEdge == .Left    ? leftArrow : leftSeparator
+                top    = (tile as! InitialTile).exitEdge == .Top     ? topArrow : topSeparator
+                bottom = (tile as! InitialTile).exitEdge == .Bottom  ? bottomArrow : bottomSeparator
             default:
-                right = "|"; left = "|"; top = "¯"; bottom = "_"
+                right = rightSeparator ; left = leftSeparator ; top = topSeparator ; bottom = bottomSeparator
                 break
             }
-            topHorizontalSpace += "|¯¯\(top)¯¯||"
+            topHorizontalSpace += "|\(topSeparator + topSeparator + top + topSeparator + topSeparator)||"
             horizontalvalues += "\(left)  \(stateSpaceId(tile))  \(right)|"
-            bottomHorizontalSpace += "|__\(bottom)__||"
+            bottomHorizontalSpace += "|\(bottomSeparator + bottomSeparator + bottom + bottomSeparator + bottomSeparator)||"
         }
+ 
         
         print(topHorizontalSpace)
         print(horizontalvalues)
