7d7246551bd891d42edc64632a5d7e828ce5b7ee
Mohamed Diaa <mohamed.diaa27@gmail.com>
2015-11-05 17:31:36 +0200

    fixin blocks count

--- RollTheBall/Utils/constants.swift
+++ RollTheBall/Utils/constants.swift
@@ -10,8 +10,8 @@ import Foundation
 
 let MIN_ROWS = 3
 let MIN_COLS = 3
-let MAX_ROWS = 3
-let MAX_COLS = 3
+let MAX_ROWS = 5
+let MAX_COLS = 5
 
 let TILE_CONFIG: [[Edge]] = [
     [.Left, .Right],
